515bd9fd5dc4bf03f8199a95ccaee8dd8564974b
Aalok766626 <aaloksah766626@gmail.com>
2020-05-25 06:49:21 -0400

    Updates

---
+++ precache-manifest.9b8e216814ef96db7d2899d678171388.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df7ebcd2256e83711a3a4cad50ef275",
+    "url": "/JobPro/index.html"
+  },
+  {
+    "revision": "6237086f10fd38dcd9b0",
+    "url": "/JobPro/static/css/2.ff6b1218.chunk.css"
+  },
+  {
+    "revision": "cc25fc0e213dd3e87e9d",
+    "url": "/JobPro/static/css/main.cc559119.chunk.css"
+  },
+  {
+    "revision": "6237086f10fd38dcd9b0",
+    "url": "/JobPro/static/js/2.bb84f081.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/JobPro/static/js/2.bb84f081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc25fc0e213dd3e87e9d",
+    "url": "/JobPro/static/js/main.a4b4675c.chunk.js"
+  },
+  {
+    "revision": "7d755fb355a23ee33a07",
+    "url": "/JobPro/static/js/runtime-main.0f258146.js"
+  },
+  {
+    "revision": "bcba0613795af42f84a56f141eadc7ac",
+    "url": "/JobPro/static/media/back.bcba0613.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/JobPro/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/JobPro/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/JobPro/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/JobPro/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/JobPro/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
